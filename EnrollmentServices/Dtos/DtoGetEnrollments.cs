8b6c60d2297ba57dc32773ed9d0c999fc1c069a5
mystald <hilmanabdan28@gmail.com>
2022-01-11 15:24:54 +0700

    Add DAL for Course and Student
    Added Dto for GetEnrollment, and custom exception for empty data

---
+++ EnrollmentServices/Dtos/DtoGetEnrollments.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EnrollmentServices.Models;
+
+namespace EnrollmentServices.Dtos
+{
+    public class DtoGetEnrollments
+    {
+        public string Code { get; set; }
+        public string CourseName { get; set; }
+        public int? Credit { get; set; }
+        public DateTime EnrollDate { get; set; }
+        public Grade Grade { get; set; }
+    }
+}
