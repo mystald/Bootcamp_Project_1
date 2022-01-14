42d98ca98c71bf0582da0c1edec980f7379db978
mystald <hilmanabdan28@gmail.com>
2022-01-14 09:57:51 +0700

    Init

---
+++ Case-2/LoggingApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoggingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
